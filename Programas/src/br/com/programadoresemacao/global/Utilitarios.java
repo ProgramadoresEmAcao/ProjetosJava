a958809e9cc552ebee4c6221793d0c05535134ab
Wiliam Dal Cim <Wileleh@gmail.com>
2014-11-14 23:49:51 -0200

    Alteracoes na classe Utilitarios

--- Programas/src/br/com/programadoresemacao/global/Utilitarios.java
+++ Programas/src/br/com/programadoresemacao/global/Utilitarios.java
@@ -9,6 +9,38 @@ public class Utilitarios {
 		} catch (Exception e) {
 			return false;
 		}
-		
 	}
+	
+	public static boolean eInteiro(String teste){
+		try {
+			Integer.valueOf(teste);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+	
+	public static String retiraMascara(String str){
+		// RETIRA ()-,."\/:';
+		str = str.replace("(", "").replace(")", "").replace("-", "").replace(",", "").replace(".", "").replace("\"", "").replace("\\", "").replace("/", "").replace(":", "").replace("'", "").replace(";", "").trim();
+		return str.trim().replaceAll("\\s+"," ");
+	}
+	
+	public static String retira(String str, String[] replace){
+		for (String string : replace) {
+			str = str.replace(string, "");
+		}
+		return str.trim().replaceAll("\\s+"," ");
+	}
+	
+	public static String toMontainCase(String str){
+		String[] cadaPalavra = str.trim().replaceAll("\\s+"," ").split(" ");
+		str = "";
+		for (String string : cadaPalavra) {
+			str += " " + string.substring(0,1).toUpperCase() + string.substring(1);
+		}
+		return str.trim();
+	}
+	
+	
 }
